cbb9f841c020b50a90a9242519e0ec06bd78706c
nerydyego <dyegonery@gmail.com>
2023-10-11 16:56:44 -0300

    Inicio de Migração e super usuario com django

--- Modulo_7/projeto/app/models.py
+++ Modulo_7/projeto/app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from datetime import datetime as dt
 
-# Create your models here.
+class ProjetoDjango(models.Model):
+    # Aqui definimos as colunas que teremos em nosso banco de dados
+    titulo = models.CharField(max_length=50)
+    descricao = models.TextField()
+    numero_de_modulos = models.DecimalField(max_digits=1000, decimal_places=2)
+    data = models.DateTimeField("Publicado em", default=dt.now())
